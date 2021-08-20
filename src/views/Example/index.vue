83aa892d633844ca40dcd07668fddc83408c9ad8
Mario <marioworker@163.com>
2021-08-20 09:21:38 +0800

    chore: init project

---
+++ src/views/Example/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="example"></div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {}
+  },
+  created() {},
+  mounted() {},
+  computed: {},
+  watch: {},
+  methods: {},
+  destroyed() {}
+}
+</script>
+<style lang="less" scoped></style>
