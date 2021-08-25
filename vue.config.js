2bfb0cf68552f046d48b1df702245b6ffd7787ae
Mario <marioworker@163.com>
2021-08-25 10:35:41 +0800

    fix: 修改打包配置

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'development' ? '' : '/gh-pages/',
+  publicPath: process.env.NODE_ENV === 'development' ? '' : '/Gdmap/',
   configureWebpack: (config) => {
     return {
       name: '基于高德地图2.0、Vue2.0',
